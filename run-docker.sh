ef24b2796c875ae0abe5648623106717abd8deba
Ziska Zarkasyi <ziska.zarkasyi@uangteman.com>
2020-03-29 12:07:00 +0700

    add docker_opts into /etc/default/docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -z "$DOCKER_OPTS" ]; then
+    echo "DOCKER_OPTS=\"$DOCKER_OPTS\"" >> /etc/default/docker
+    echo "Adding DOCKER_OPTS to /etc/default/docker"
+fi
+
 if [ "$DOCKER_IN_DOCKER" = "start" ] ; then
  rm /var/run/docker.pid 2>/dev/null
  service docker start
